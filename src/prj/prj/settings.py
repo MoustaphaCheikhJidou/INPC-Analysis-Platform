13c79b3d3b1e11146856cf6587b86cf709450cf0
MoustaphaCheikhJidou <20226@isms.esp.mr>
2025-01-24 11:31:38 +0000

    update 2

--- src/prj/prj/settings.py
+++ src/prj/prj/settings.py
@@ -147,3 +147,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'elmoustapha.cheikh.jiddou@gmail.com'
 EMAIL_HOST_PASSWORD = 'rmjt ndxq amwi oarc'
+CSRF_TRUSTED_ORIGINS = ['http://138.201.52.29:25231']'
