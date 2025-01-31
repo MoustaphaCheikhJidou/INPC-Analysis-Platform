1be23368f42f0827bd18fc62321174ca11596db2
MoustaphaCheikhJidou <20226@isms.esp.mr>
2025-01-31 02:29:31 +0000

    Phase 3

--- src/prj/prj/settings.py
+++ src/prj/prj/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-ighi#_@d7)4f#1ba(=z(xrtx$ze_-pbmsu*t+p$p7mp#vlw_70
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    'teaching.sidi.xyz',
+    '138.201.52.29',
+    # Ajoutez d'autres hôtes si nécessaire
+]
 
 
 # Application definition
@@ -39,17 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'users',  # Assurez-vous que cette ligne est présente
-        "crispy_forms", 
-    "crispy_bootstrap4",
+    'crispy_forms', 
+    'crispy_bootstrap4',
     'chartjs',
-    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',  # Assurez-vous que ce middleware est présent
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -60,12 +63,12 @@ ROOT_URLCONF = 'prj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # Assurez-vous que le chemin est correct
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # Nécessaire pour certains contextes
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -79,17 +82,16 @@ WSGI_APPLICATION = 'prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
-        "USER": "django_user",
-        "PASSWORD": "django_password",
-        "HOST": "mydb_23231",
-        "PORT": "3306",
-        "CHARSET": "UTF8",
+        'USER': 'django_user',
+        'PASSWORD': 'django_password',
+        'HOST': 'mydb_23231',
+        'PORT': '3306',
         'OPTIONS': {
+            'charset': 'utf8mb4',  # Correction : 'charset' au lieu de 'CHARSET'
             "init_command": "SET default_storage_engine=INNODB",
         },
     }
@@ -130,23 +132,38 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Ajout de la barre oblique pour la cohérence
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 LOGIN_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL ='/media/'
+MEDIA_URL = '/media/'  # Correction : Ajout de la barre oblique au début
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4" 
-LOGIN_URL='login'
+LOGIN_URL = 'login'
+
+# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'elmoustapha.cheikh.jiddou@gmail.com'
-EMAIL_HOST_PASSWORD = 'rmjt ndxq amwi oarc'
-CSRF_TRUSTED_ORIGINS = ['http://138.201.52.29:25231',' http://teaching.sidi.xyz:25231']
+EMAIL_HOST_PASSWORD = 'rmjt ndxq amwi oarc'  # **Sécurité : Utilisez des variables d'environnement pour les mots de passe**
+
+# CSRF Configuration
+CSRF_TRUSTED_ORIGINS = [
+    'http://138.201.52.29:25231',
+    'http://teaching.sidi.xyz:25231',
+]
+
+# Sécurité Additionnelle (Optionnel)
+# Ajoutez des paramètres de sécurité si vous êtes en production
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
